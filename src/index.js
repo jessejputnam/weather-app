6c6e923ddab39219ca371bb1ee050810fa17f6cd
Jesse Putnam <jessejputnam@gmail.com>
2022-04-19 11:15:10 -0400

    Add geolocation retrieval and async weather data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 "use strict";
+
+const testBtn = document.querySelector("#test-btn");
+
+const getCoords = async function () {
+  const getCurrentPosition = function () {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => resolve(position),
+        (error) => reject(error)
+      );
+    });
+  };
+
+  const position = await getCurrentPosition();
+  const { latitude: lat, longitude: long } = position.coords;
+  return { lat, long };
+};
+
+const getWeatherData = async function () {
+  const coords = await getCoords();
+  const response = await fetch(
+    `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.long}&appid=e1c7899ad76e2db415e336ec95e711cd`
+  );
+  const data = await response.json();
+  //! has weather, now need promise.all() for api of city name as well
+  console.log(data);
+};
+
+getWeatherData();
+
+testBtn.addEventListener("click", async () => {
+  console.log(await getCoords());
+});
